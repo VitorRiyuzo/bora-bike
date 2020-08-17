a1705f09a4f1786f5243d80c2d33eef575c89f2d
VitorRiyuzo <vitor_riyuzo@hotmail.com>
2020-08-17 10:57:40 -0300

    ajuste na url de publicação

--- src/index.js
+++ src/index.js
@@ -2,29 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
-
-// Desafio master blaster na descrição
-// Colocar um jogo ou brincadeira nessa página:
 const Pagina404 = () => (
   <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-    <h1>Ops, quebrou tudo :O</h1>
-    
+    <h1>Ops, Página não encontrada</h1>
   </div>
 )
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
     </Switch>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
